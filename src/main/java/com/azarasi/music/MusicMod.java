037b9332b124639703475050e34dcfba8f54af2a
sohhi0654 <goma513@outlook.jp>
2026-08-29 14:05:37 +0900

    Refactor command registration and player handling

--- src/main/java/com/azarasi/music/MusicMod.java
+++ src/main/java/com/azarasi/music/MusicMod.java
@@ -3,12 +3,11 @@ package com.azarasi.music;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
 
 public class MusicMod implements DedicatedServerModInitializer {
-
     @Override
     public void onInitializeServer() {
         // Lavaplayerオーディオマネージャーの初期化
@@ -16,23 +15,21 @@ public class MusicMod implements DedicatedServerModInitializer {
 
         // /play <URL> コマンドの登録
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            dispatcher.register(CommandManager.literal("play")
-                .then(CommandManager.argument("url", StringArgumentType.string())
+            dispatcher.register(Commands.literal("play")
+                .then(Commands.argument("url", StringArgumentType.string())
                     .executes(context -> {
                         String url = StringArgumentType.getString(context, "url");
                         var server = context.getSource().getServer();
-                        ServerPlayerEntity player = context.getSource().getPlayer();
-
+                        ServerPlayer player = context.getSource().getPlayer();
                         String senderName = player != null ? player.getName().getString() : "サーバー";
 
                         // 全員に再生リクエストのチャット通知
-                        server.getPlayerManager().broadcast(
-                            Text.literal("♪ " + senderName + " が曲を再生リクエストしたよ！"), false
+                        server.getPlayerList().broadcastSystemMessage(
+                            Component.literal("♪ " + senderName + " が曲を再生リクエストしたよ！"), false
                         );
 
                         // 非同期で音声ストリームを取得して再生
                         AudioManager.playUrl(url, server);
-
                         return 1;
                     })
                 )
